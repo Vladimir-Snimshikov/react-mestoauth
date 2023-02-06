c70597bfaca5785082b990281e69dc4f1d7aac15
Vladimir Snimshikov <vladimirsnimshikov@yandex.ru>
2023-02-06 16:25:09 +0400

    refactor:вынести className элементов и текс кнопок формы в переменные

---
+++ src/utils/constans.js
@@ -0,0 +1,10 @@
+export const elemClasses = {
+  popupOpened: 'popup_opened',
+  popupExitBbutton: 'popup__exit-button',
+};
+export const saveButtonText = {
+  textRemoval: 'Удаление...',
+  textToCreate: 'Создать',
+  textSave: 'Сохранить',
+  textConservation: 'Сохранение...',
+};
